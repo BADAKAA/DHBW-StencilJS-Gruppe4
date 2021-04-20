97b2e56ee68bc8da3bb1f29d377b0ac887d1f0f5
BADAKAA <81486945+BADAKAA@users.noreply.github.com>
2021-04-20 22:07:31 +0200

    add slider function

--- src/components/image-slider/image-slider.tsx
+++ src/components/image-slider/image-slider.tsx
@@ -1,5 +1,6 @@
 //This tutorial is the basis for most of the sliuder-logic:
 //https://www.youtube.com/watch?v=KcdBOoK3Pfw
+//Ich habe das Tutorial nur bis Minute 9:02 angesehen 
 
 
 import { Component, Host, Prop, h } from '@stencil/core';
@@ -7,8 +8,8 @@ let imageElements:Array<HTMLImageElement>=[];
 let sliderFrame:HTMLDivElement;
 let slider:HTMLDivElement;
 
-let currentImageNumber:number=1;
-let sliderSize
+let currentImageNumber:number=2;
+let sliderSize:number;
 @Component({
   tag: 'image-slider',
   styleUrl: 'image-slider.css',
@@ -45,11 +46,29 @@ export class ImageSlider {
 function addImages() {
 
   setTimeout(()=>{
-  sliderFrame=document.querySelector("image-slider").shadowRoot.querySelector("#slider");
+
+  sliderFrame=document.querySelector("image-slider").shadowRoot.querySelector("#slider-frame");
   slider=document.querySelector("image-slider").shadowRoot.querySelector("#slider");
-  for (let image of imageElements) {
-    console.log(slider);
-    slider.appendChild(image);
+
+  sliderSize=sliderFrame.clientWidth;
+  //add last image to start and first image to end to enable smooth transition
+  //.cloneNode(true) is necessary because the elements won't get appended multiple times otherwise
+  slider.appendChild(imageElements[imageElements.length-1]);
+  
+  //append images to slider
+  for (let image of imageElements) {    
+    slider.appendChild(image.cloneNode(true));
   }
+  slider.appendChild(imageElements[0]).cloneNode(true);
+  //move slider to hide last image appended to start for smooth transition
+  slider.style.transform = "translateX("+ (-sliderSize) +"px)";
+  slider.style.transition= "transform 2s";
+  setInterval(()=>{changeSlide()},5000);
   },0)
+}
+
+
+function changeSlide() {
+  slider.style.transform = "translateX("+ (-sliderSize*currentImageNumber) +"px)";
+  currentImageNumber++;
 }
