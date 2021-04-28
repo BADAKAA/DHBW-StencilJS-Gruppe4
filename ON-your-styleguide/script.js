ccd5bd19424d596f4b21ba9b2ed3108253ba79b9
Anna-Maria Haslinger <anna-maria.haslinger@gmx.de>
2021-04-28 17:34:21 +0200

    CSS bisschen angepasst

--- ON-your-styleguide/script.js
+++ ON-your-styleguide/script.js
@@ -1,9 +1,9 @@
 document.querySelector('example-component').addEventListener('exampleEvent', () => console.log('test'));
 //date-picker
 const calendar = document.querySelector('my-component');
-  calendar.dayNames = [
-    'Mo', 'Tu', 'We', 'Th', 'Fr', 'Sa', 'So'
-  ];
-  calendar.monthNames = [
-    'January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'
-  ];
+calendar.dayNames = [
+  'Mo', 'Di', 'Mi', 'Do', 'Fr', 'Sa', 'So'
+];
+calendar.monthNames = [
+  'January', 'Februar', 'März', 'April', 'Mai', 'Juni', 'Juli', 'August', 'September', 'Oktober', 'November', 'Dezember'
+];
