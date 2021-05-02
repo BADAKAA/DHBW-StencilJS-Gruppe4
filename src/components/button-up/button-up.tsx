eddd8550d6643fd1e619efe9470a0b167c0b51c1
fraukejr <fraukeridder@icloud.com>
2021-05-02 22:42:51 +0200

    change CSS

--- src/components/button-up/button-up.tsx
+++ src/components/button-up/button-up.tsx
@@ -20,7 +20,7 @@ export class ButtonUp {
 
   backToTop(){
     console.log("klappt");
-    window.scroll({ top: 0, left: 0});
+    window.scroll({ top: 0, left: 0, behavior:'smooth'});
   }
 
   render() {
