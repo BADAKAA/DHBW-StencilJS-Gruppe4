beb6c59e5ef128902f019867ea6411952796107b
lena-3 <lena.ernst2000@web.de>
2021-05-03 20:57:53 +0200

    change class on p element

--- src/components/cookie-banner/cookie-banner.tsx
+++ src/components/cookie-banner/cookie-banner.tsx
@@ -30,7 +30,7 @@ export class CookieBanner {
             <button id="closeButton" class="buttons">&#10005;</button>
             <p class="heading">{this.heading && <p>{this.heading}</p>}</p>
             <p>{this.bannertext && <p>{this.bannertext}</p>}</p>
-            <p onClick={() => this.linkGuidelines()} class="link">{this.linkguidelines && <p>{this.linkguidelines}</p>}</p>
+            <p onClick={() => this.linkGuidelines()}>{this.linkguidelines && <p class="link">{this.linkguidelines}</p>}</p>
             <button id="acceptButton" class="buttons">{this.buttontext && <p>{this.buttontext}</p>}</button>
           </div>
         </div>
