dd0dfdd772f526f798f5f92cab28c39a121090c4
BADAKAA <81486945+BADAKAA@users.noreply.github.com>
2021-05-06 00:59:06 +0200

    add images to event list

--- src/components/event-list/event-list.tsx
+++ src/components/event-list/event-list.tsx
@@ -1,11 +1,24 @@
 import { Component, Host, Element, Prop, h } from '@stencil/core';
 
-let eventData: any;
+let eventData: Array<event>;
 
 let componentElement: ShadowRoot;
 let listElement: HTMLUListElement;
 let expanded: boolean = false;
 let previousYears: Array<number> = [];
+
+export interface event {
+  title:string,
+  date:string,
+  start?:string,
+  end?:string,
+  city?:string,
+  location?:string,
+  adress?:string,
+  description?:string,
+  image?:string
+}
+
 @Component({
   tag: 'event-list',
   styleUrl: 'event-list.css',
@@ -50,58 +63,52 @@ function fillList() {
 
 
     const date = new Date(event.date);
-    checkIfYearIsNew(date)
+    console.log(convertDate(date));
+    
+    checkIfYearIsNew(date);
 
     const listItem: HTMLElement = document.createElement("LI");
-    /*
-    const dateFrame:HTMLDivElement=document.createElement("DIV") as HTMLDivElement;
-    dateFrame.className="date";
-    dateFrame.innerHTML=`<small>${getMonth(date)}</small> <p class="dateNumber">${getDay(date)}</p>`;
-    listItem.appendChild(dateFrame);
 
-    const eventFrame:HTMLDivElement=document.createElement("DIV") as HTMLDivElement;
-    dateFrame.className="event";
-    dateFrame.innerHTML=`
-    <p class="time"><p class="weekday">${getDayName(date)}</p> ${event.start+"–"+event.end+" Uhr"}</p>
-    <h2 class="eventTitle">${event.title}</h2>
-    <div class="info">
-      <h3 class="city">${event.city}</h3> ${event.location}`;
-    listItem.appendChild(eventFrame);
-
-*/
-
-
-
-
-
-
-
-    listItem.innerHTML= `
-      <div class="date">
-        <small>${getMonth(date)}</small>
-        <p class="dateNumber">${getDay(date)}</p>
+    const listItemContent:string= `
+    <div class="date">
+      <small>${getMonth(date)}</small>
+      <p class="dateNumber">${getDay(date)}</p>
+    </div>
+    <div class="event">
+        <h5 class="time"><p class='weekday'>${event.start+' – '+event.end+' Uhr'}</p></h5>
+      <h2 class="eventTitle">${event.title}</h2>
+      <div class="info">
+        <h3 class="city">${event.city}</h3> ${event.location}
+      <p class="description">${event.description}<p>
       </div>
-      <div class="event">
-        <p class="time"><p class='weekday'>${getDayName(date)}</p> ${event.start+' – '+event.end+' Uhr'}</p>
-        <h2 class="eventTitle">${event.title}</h2>
-        <div class="info">
-          <h3 class="city">${event.city}</h3> ${event.location}
-        </div>
-        <div class="details" style='display:none'>
-          <p><h4>Location: </h4>${event.location}</p>
-          <p><h4>Date: </h4>${(convertDate(date))}</p>
-          <p><h4>Time: </h4>${event.time} Uhr</p>
-          <p><h4>Description: </h4>${event.description}<p>
-        </div>
+      <div class="details" style='display:none'>
+        <h4 class="adress"><p class="detail">Adress 📍</p>${event.adress}</h4>
+        <h4 class="adress"><p class="detail">Location </p>${event.location}</h4>
+        <h4 class="clickDate"><p class="detail">Date</p>${getDayName(date)+", "+(convertDate(date))}</h4>
+        <h4><p class="detail">Time</p>${event.start} – ${event.end} Uhr </h4> 
+        <h4 style="margin-top:0.5em"><p class="detail">Description</p></h4>
+        <p class="description">${event.description}<p>
       </div>
-      <div class="image">
-      </div>`
+    </div>
+    <div class="eventImage" style="background-image:${"url("+getImageLink(event)+")"}">
+    </div>`
+
+    listItem.innerHTML= listItemContent;
     listItem.addEventListener("click", () => expandItem(listItem));
+    listItem.querySelectorAll(".adress").forEach(a=> a.addEventListener("click", () => openAdress(event.adress)));
+    listItem.querySelector(".clickDate").addEventListener("click", () => openDate(event.date));
     listElement.appendChild(listItem);
   }
-
 }
 
+function getImageLink(event:event) {
+  if(!event.image) {
+    //retun link to stock image in case no image link was specified.
+    return "https://images.unsplash.com/photo-1529651737248-dad5e287768e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1901&q=80";
+  }
+  return event.image;
+
+}
 function checkIfYearIsNew(date: Date) {
 
   const currentYear = date.getFullYear();
@@ -119,7 +126,22 @@ function checkIfYearIsNew(date: Date) {
   }
 }
 
+function openAdress(adress:string) {
 
+    const url = 'https://www.google.com/maps/place/' + adress;
+    window.open(url, '_blank');
+}
+
+
+function openDate(input:string) {
+
+  const date:Date= new Date(input);
+  const day:string= date.getFullYear() + "/" + date.getMonth() + "/" +  getDay(date);
+  const url = 'https://calendar.google.com/calendar/u/1/r/day/' + day;
+  console.log(url);
+  
+  window.open(url, '_blank');
+}
 function expandItem(listItem: HTMLElement) {
 
   const info = listItem.querySelector(".info") as HTMLElement;
@@ -128,16 +150,18 @@ function expandItem(listItem: HTMLElement) {
   if (details.style.display == "none") {
     info.style.display = "none";
     details.style.display = "contents";
+    listItem.style.cursor="auto"
   } else {
     info.style.display = "contents";
     details.style.display = "none";
+    listItem.style.cursor="pointer";
   }
 }
 
 
 function convertDate(date: Date) {
 
-  return getDay(date) + "." + date.getMonth() + "." + date.getFullYear();
+  return date.getDate() + "." + (date.getMonth()+1).toString()  + "." + date.getFullYear();
 }
 
 function getDayName(date: Date, short?: boolean): string {
@@ -161,8 +185,8 @@ function getMonth(date: Date): string {
   ];
   return monthNames[date.getMonth()]
 }
+
 //This code adds a buttons to collapse or expand all elements and to sort items.
-//It is used by calling the  the addButton() function in componentDidLoad().
 let detailButton: HTMLElement;
 let sortButton: HTMLElement;
 
@@ -193,20 +217,9 @@ function expandAllItems() {
   const listItems = componentElement.querySelectorAll("LI") as unknown as Array<HTMLElement>;
 
   for (const listItem of listItems) {
-
-    const info = listItem.querySelector(".info") as HTMLElement;
-    const details = listItem.querySelector(".details") as HTMLElement;
-
-    if (!expanded) {
-      info.style.display = "none";
-      details.style.display = "contents";
-      detailButton.textContent = "Hide Details";
-    } else if (expanded) {
-      info.style.display = "contents";
-      details.style.display = "none";
-      detailButton.textContent = "Show Details";
-    }
+    expandItem(listItem);
   }
+
   expanded = !expanded;
 }
 
@@ -254,7 +267,7 @@ function updateLayout() {
   {
     //Images disapper if viewport width is less than 1000px.
     let imageDisplay: string = "contents";
-    let gridStyle: string = "1fr 5fr 3fr";
+    let gridStyle: string = "1.5fr 9fr 6fr";
   
     if (window.innerWidth < 1000) {
       imageDisplay = "none";
